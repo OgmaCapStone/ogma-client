3aa66d5673964cfed59c9ff1f09c98fbf85e7b96
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-06-17 21:15:12 -0500

    ➕ADD : Navbar component. The navbar component has been created. Also, the file structure to contain all the assest have been created into the src folder.

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+    <section className="navbar">
+      <section className="navbar-brand">
+        <a href=""></a>
+        <img src="" alt="" />
+      </section>
+      <section className="navbar-buttons">
+        <button>Log In</button>
+        <button>Start Now</button>
+      </section>
+    </section>
+  )
+}
+
+export default Navbar
