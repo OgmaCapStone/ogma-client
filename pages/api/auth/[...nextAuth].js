a84e9f9e86186dca1154d0a3791eeb5d989189e3
Juan Daniel Martinez <mtzn_juandaniel@yahoo.com.mx>
2021-06-18 11:08:29 -0600

    ➕ ADD: storing profile data on session callback. Initializing next-auth session callback to store basic session info when a user logs in via Google or GitHub.

--- pages/api/auth/[...nextAuth].js
+++ pages/api/auth/[...nextAuth].js
@@ -9,16 +9,29 @@ const providers = [
     name: "Google",
     clientId: process.env.GOOGLE_ID,
     clientSecret: process.env.GOOGLE_SECRET,
+    profile(profile) {
+      return { ...profile, image: profile.picture };
+    },
   }),
   Providers.GitHub({
     name: "GitHub",
     clientId: process.env.GITHUB_ID,
     clientSecret: process.env.GITHUB_SECRET,
+    profile(profile) {
+      return { ...profile, image: profile.avatar_url };
+    },
   }),
 ];
 
+const callbacks = {
+  async session(session) {
+    return session;
+  },
+};
+
 const options = {
   providers,
+  callbacks,
 };
 
 export default (req, res) => NextAuth(req, res, options);
