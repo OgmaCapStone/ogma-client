35245484115e6753e609fbd4568564b721b6081a
Juan Daniel Martinez <mtzn_juandaniel@yahoo.com.mx>
2021-06-18 15:58:28 -0600

    ➕ ADD: layout and login styles. Styling layout background, missing media queries. Also, the login container has been styled.

--- pages/login.js
+++ pages/login.js
@@ -2,6 +2,8 @@ import React, { useReducer } from "react";
 import Link from "next/link";
 import { initialState, reducer } from "@reducers/login";
 import { GithubIcon, GoogleIcon } from "@icons";
+import Layout from "@components/Layout";
+import styles from "@styles/Login.module.scss";
 
 export default function login() {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -11,40 +13,42 @@ export default function login() {
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">
-          Email
-          <input
-            name="email"
-            type="email"
-            value={state.email}
-            onChange={(e) => dispatch({ type: "change", e })}
-          />
-        </label>
-        <label htmlFor="password">
-          Password
-          <input
-            name="password"
-            type="password"
-            value={state.password}
-            onChange={(e) => dispatch({ type: "change", e })}
-          />
-        </label>
-        <button type="submit">Log In</button>
-      </form>
-      <p>or</p>
-      <div>
-        <button type="button">
-          <GoogleIcon size={16} /> Google
-        </button>
-        <button type="button">
-          <GithubIcon size={16} /> GitHub
-        </button>
+    <Layout>
+      <div className={styles.login__container}>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">
+            Email
+            <input
+              name="email"
+              type="email"
+              value={state.email}
+              onChange={(e) => dispatch({ type: "change", e })}
+            />
+          </label>
+          <label htmlFor="password">
+            Password
+            <input
+              name="password"
+              type="password"
+              value={state.password}
+              onChange={(e) => dispatch({ type: "change", e })}
+            />
+          </label>
+          <button type="submit">Log In</button>
+        </form>
+        <p>or</p>
+        <div>
+          <button type="button">
+            <GoogleIcon size={16} /> Google
+          </button>
+          <button type="button">
+            <GithubIcon size={16} /> GitHub
+          </button>
+        </div>
+        <p>
+          Don&apos;t have an account? <Link href="/signup">Sign up</Link>
+        </p>
       </div>
-      <p>
-        Don&apos;t have an account? <Link href="/signup">Sign up</Link>
-      </p>
-    </div>
+    </Layout>
   );
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import styles from "@styles/Layout.module.scss";
 
 export default function Layout({ children, header, footer }) {
   return (
-    <>
+    <div className={styles.layout__background}>
       {header && <header>Ogma app</header>}
       {children}
       {footer && <footer>&copy; 2021 | Ogma app</footer>}
-    </>
+      <div
+        className={`${styles.layout__circle} ${styles.layout__circle_top}`}
+      />
+      <div
+        className={`${styles.layout__circle} ${styles.layout__circle_bottom}`}
+      />
+    </div>
   );
 }
