14512838b2861ddbb5386ba795cbc6fb1b421931
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-06-19 11:05:06 -0500

    ♻️ REFACTOR : .env & json config edited. Environment varibles added to file for auth, json config files edited for path alias & others.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
-import styles from '../Navbar/navbar.module.scss'
-
+import { OgmaLogoIcon } from '@icons'
+import styles from '@styles/Navbar.module.scss'
 
 const Navbar = () => {
   return (
     <section className={styles.header}>
       <section className={styles.navbar}>
-        <section className={styles.navbarBrand}>
-          <h1>Ogma</h1>
-        </section>
-        <section className={styles.btnNavbar}>
-          <button className={styles.btnLogin}>Log In</button>
-          <button className={styles.btnStart}>Start Now</button>
+        <section className={styles.navbar_brand}>
+          <OgmaLogoIcon size={40} color="#FFF"/>
         </section>
       </section>
     </section>
