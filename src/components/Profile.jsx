a3db3249ae18e63424dd9f2ca11eb2847f59e6ef
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-06-19 23:05:09 -0500

    ➕ADD : Profile component created.
    Profile component & views files created.

---
+++ src/components/Profile.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '@components/Navbar'
+import Footer from '@components/Footer'
+
+const ProfileView = () => {
+  return (
+    <div>
+      <Navbar/>
+      Hello World
+      <Footer/>
+    </div>
+  )
+}
+
+export default ProfileView
