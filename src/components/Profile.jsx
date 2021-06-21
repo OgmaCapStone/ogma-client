833c3f85a20eade529fa03e4e01626386bff8cd2
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-06-21 18:47:03 -0500

    ♻️ REFACTOR : Profile component styled for mobile.
    Profile component styled for mobile screens, most of the view is complete, missing some minor details.

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,15 +1,38 @@
 import React from 'react'
 import Navbar from '@components/Navbar'
 import Footer from '@components/Footer'
+import styles from '@styles/Profile.module.scss'
 
-const ProfileView = () => {
+const Profile = () => {
   return (
-    <div>
+    <section>
       <Navbar/>
-      Hello World
+      <section className={styles.main_container}>
+        <section className={styles.profile_container}>
+          <section className={styles.profile_header}>
+            {/* <img src="/images/double-bubble.png" alt="bg-pattern" id={styles.bg_pattern}/> */}
+          </section>
+            <img src="/images/Crop Photo LinkedIn.jpeg" id={styles.profile_pic}/>
+          <section className={styles.profile_content}>
+            <section className={styles.profile_name}>
+              <h1>Juan Martínez</h1>
+            </section>
+            <section className={styles.profile_sub}>
+              <span id={styles.level}>Mid Developer</span>
+              <span id={styles.techs}>Python • Javascript</span>
+            </section>
+            <section className={styles.profile_btn}>
+              <button className={styles.start_btn}>Start now</button>
+            </section>
+          </section>
+        </section>
+        <section className={styles.skills_container}>
+          <h1>Skills</h1>
+        </section>
+      </section>
       <Footer/>
-    </div>
+    </section>
   )
 }
 
-export default ProfileView
+export default Profile
