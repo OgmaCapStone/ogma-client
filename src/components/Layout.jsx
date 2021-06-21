85f39cd5e7bc3c62f093edaff9e4f059f8ee4ab9
Juan Daniel Martinez <mtzn_juandaniel@yahoo.com.mx>
2021-06-21 16:04:26 -0600

    ♻ REFACTOR: App background and container styles. Removing Layout styles and replacing it with a global SASS class named .container to manage all sizes in different breakpoints.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,20 +1,13 @@
 import React from "react";
 import Header from "@components/Navbar";
 import Footer from "@components/Footer";
-import styles from "@styles/Layout.module.scss";
 
 export default function Layout({ children, header, footer }) {
   return (
-    <div className={styles.layout__background}>
+    <>
       {header && <Header />}
-      {children}
+      <main className="main container">{children}</main>
       {footer && <Footer />}
-      <div
-        className={`${styles.layout__circle} ${styles.layout__circle_top}`}
-      />
-      <div
-        className={`${styles.layout__circle} ${styles.layout__circle_bottom}`}
-      />
-    </div>
+    </>
   );
 }
