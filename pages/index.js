bb4139fdba7c54eca0d937a3ffa8ba7f294c699e
Elsa Portilla <elsa.portilla@gmail.com>
2021-06-22 13:57:51 -0500

    ♻️ REFACTOR: added media breakpoints for tablet and desktop

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Layout from "@components/Layout";
 import { OgmaLogoIcon } from "@icons";
-import styles from "../styles/Index.module.scss";
+import styles from "@styles/Index.module.scss";
 
 export default function Home() {
   return (
@@ -18,18 +18,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <div className={styles.heroImage}>
+      <main className={styles.landing__main}>
+        <div className={styles.landing__heroImage}>
           <Image width={580} height={470} src="/public/images/heroImage.png" />
         </div>
-        <div className={styles.heroInfo}>
-          <h1 className={styles.title}>Welcome to</h1>
+        <div className={styles.landing__heroInfo}>
+          <h1 className={styles.landing__title}>Welcome to</h1>
           <OgmaLogoIcon size={60} color="#6da8aa" />
-          <p className={styles.text}>Practice a tech language for free!</p>
-          <button type="button" className={styles.primaryBtn}>
+          <p className={styles.landing__text}>Practice a tech language for free!</p>
+          <button type="button" className={styles.landing__primaryBtn}>
             <Link href="/signin">Create account</Link>
           </button>
-          <button type="button" className={styles.primaryBtn}>
+          <button type="button" className={styles.landing__primaryBtn}>
             <Link href="/login">Log in</Link>
           </button>
         </div>
