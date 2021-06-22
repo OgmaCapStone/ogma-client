461b34c67056e67ec0c7097ff4c18df5ca2f54f0
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-06-21 23:17:13 -0500

    ♻️ REFACTOR : Profile styles edited.
    Profile component edited for queries for mobile. Design & responsiveness is working for mobile.

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,37 +1,32 @@
 import React from 'react'
-import Navbar from '@components/Navbar'
-import Footer from '@components/Footer'
+import Layout from '@components/Layout'
 import styles from '@styles/Profile.module.scss'
 
 const Profile = () => {
   return (
-    <section>
-      <Navbar/>
-      <section className={styles.main_container}>
-        <section className={styles.profile_container}>
-          <section className={styles.profile_header}>
-            {/* <img src="/images/double-bubble.png" alt="bg-pattern" id={styles.bg_pattern}/> */}
-          </section>
-            <img src="/images/Crop Photo LinkedIn.jpeg" id={styles.profile_pic}/>
-          <section className={styles.profile_content}>
-            <section className={styles.profile_name}>
-              <h1>Juan Martínez</h1>
-            </section>
-            <section className={styles.profile_sub}>
-              <span id={styles.level}>Mid Developer</span>
-              <span id={styles.techs}>Python • Javascript</span>
-            </section>
-            <section className={styles.profile_btn}>
-              <button className={styles.start_btn}>Start now</button>
-            </section>
-          </section>
+    <Layout header footer>
+      <section className={styles.profile_container}>
+        <section className={styles.profile_header}>
+          {/* <img src="/images/double-bubble.png" alt="bg-pattern" id={styles.bg_pattern}/> */}
         </section>
-        <section className={styles.skills_container}>
-          <h1>Skills</h1>
+        <img src="/images/Crop Photo LinkedIn.jpeg" id={styles.profile_pic} />
+        <section className={styles.profile_content}>
+          <section className={styles.profile_name}>
+            <h1>Juan Martínez</h1>
+          </section>
+          <section className={styles.profile_sub}>
+            <span id={styles.level}>Mid Developer</span>
+            <span id={styles.techs}>Python • Javascript</span>
+          </section>
+          <section className={styles.profile_btn}>
+            <button className={styles.start_btn}>Start now</button>
+          </section>
         </section>
       </section>
-      <Footer/>
-    </section>
+      <section className={styles.skills_container}>
+        <h1>Skills</h1>
+      </section>
+    </Layout>
   )
 }
 
