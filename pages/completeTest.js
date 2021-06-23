4f6349dee8587c91a983d37adf65a86c7b748e87
Elsa Portilla <elsa.portilla@gmail.com>
2021-06-23 17:57:31 -0500

    ➕ADD: Complete and incomplete  quizz results layout designed for mobile

---
+++ pages/completeTest.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Link from "next/link";
+import Head from "next/head";
+import Image from "next/image";
+import Layout from "@components/Layout";
+import styles from "@styles/Results.module.scss";
+
+export default function Home() {
+  return (
+    <Layout>
+      <Head>
+        <title>Ogma App</title>
+        <meta
+          name="description"
+          content="Web app to practice for yout next job interview"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.results__main}>
+        <div className={styles.results__info}>
+          <h2 className={styles.results__title}>
+            Quiz completed!
+          </h2>
+          <h2 className={styles.results__title}>
+            You can move to the next level!
+          </h2>
+          <Image width={350} height={350} src="/images/completeDesktop.png" alt="Complete test image" />
+          <button type="button" className={styles.results__primaryBtn}>
+            <Link href="/profile">Continue</Link>
+          </button>
+        </div>
+      </main>
+    </Layout>
+  );
+}
---
+++ pages/incompleteTest.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Link from "next/link";
+import Head from "next/head";
+import Image from "next/image";
+import Layout from "@components/Layout";
+import styles from "@styles/Results.module.scss";
+
+export default function Home() {
+  return (
+    <Layout>
+      <Head>
+        <title>Ogma App</title>
+        <meta
+          name="description"
+          content="Web app to practice for yout next job interview"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.results__main}>
+        <div className={styles.results__info}>
+          <h2 className={styles.results__title}>
+            Quiz incompleted!
+          </h2>
+          <h2 className={styles.results__title}>
+            Review your notes and try again!
+          </h2>
+          <Image width={350} height={350} src="/images/incompleteDesktop.png" alt="Complete test image" />
+          <button type="button" className={styles.results__primaryBtn}>
+            <Link href="/profile">Try again</Link>
+          </button>
+          <button type="button" className={styles.results__secondaryBtn}>
+            <Link href="/profile">Go back</Link>
+          </button>
+        </div>
+      </main>
+    </Layout>
+  );
+}
