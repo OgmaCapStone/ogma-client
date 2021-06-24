d81240a0156019b2c25ab11568601734537e2250
Juan Daniel Martinez <mtzn_juandaniel@yahoo.com.mx>
2021-06-24 13:17:25 -0600

    ➕ ADD: onChange function on SelectOptions component. Getting selected value in SelectOptions component with onChange event.

--- src/components/selectOptions.jsx
+++ src/components/selectOptions.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import styles from "@styles/selectOptions.module.scss";
 
 export default function selectOptions({
+  id,
+  onChange,
   options,
   icon,
   background,
   className,
 }) {
   return (
-    <div className={`${className} ${styles.options__container}`}>
+    <div
+      onChange={onChange}
+      className={`${className || ""} ${styles.options__container}`}
+    >
       {options.map((item, index) => (
         <label htmlFor={`option-${index}`} key={`option-${index}`}>
           <input
@@ -16,7 +21,8 @@ export default function selectOptions({
             type="radio"
             name="options"
             id={`option-${index}`}
-            value=""
+            value={item.value}
+            required
           />
           <div
             className={`${styles[`options__${background || "default"}`]} ${
