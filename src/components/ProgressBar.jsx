f1fd8025afe3fbaa0ef85289570c112f52b49bde
Juan Camilo Garcés <camo0402@gmail.com>
2021-06-25 15:43:28 -0500

    ♻️ REFACTOR :Progress component's contrast color

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styles from "@styles/ProgressBar.module.scss";
 
 const ProgressBar = ({ completed }) => {
   const filler = {
+    // transition: "width 1s ease-in-out",
     width: `${completed}%`,
   };
   return (
     <div className={styles.ProgressBar}>
       <div className={styles.ProgressBar__filler} style={filler}>
         {completed}
-        {/* <span className={styles.ProgressBar__label}>{`${completed}%`}</span> */}
       </div>
       <div className={styles.ProgressBar__container}>
         <span
