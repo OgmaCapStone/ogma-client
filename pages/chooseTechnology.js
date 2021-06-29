243922e138d441e13eebef234ae9ebdecf08270b
Juan Daniel Martinez <mtzn_juandaniel@yahoo.com.mx>
2021-06-29 13:28:20 -0600

    ➕ ADD: Layout component. Wrapping the page with the Layout component.

--- pages/chooseTechnology.js
+++ pages/chooseTechnology.js
@@ -4,6 +4,7 @@ import Link from "next/link";
 import Options from "@components/SelectOptions";
 import { getTechnologies } from "@database/technologies";
 import withAuth from "@auth";
+import Layout from "@components/Layout";
 import styles from "@styles/ChooseTechnology.module.scss";
 
 function choseTechnology() {
@@ -15,31 +16,33 @@ function choseTechnology() {
   }, []);
 
   return (
-    <div className={styles.ChooseTechnology}>
-      <div className={styles.ChooseTechnology__text}>
-        <h2>Choose a technology</h2>
-        <p>
-          Practice your skills with a set of questions to improve your level.
-        </p>
+    <Layout>
+      <div className={styles.ChooseTechnology}>
+        <div className={`${styles.ChooseTechnology__text}`}>
+          <h2>Choose a technology</h2>
+          <p>
+            Practice your skills with a set of questions to improve your level.
+          </p>
+        </div>
+        <Options
+          className={`${styles.ChooseTechnology__options}`}
+          cardStyles="withImage"
+          onChange={(e) => {
+            dispatch({ type: "SET_TECHNOLOGY", technology: e.target.value });
+          }}
+          options={technology?.map((tech) => ({
+            label: tech.name,
+            subLabel: tech.summary,
+            image: `${tech.image}.png`,
+            value: tech.name.toLowerCase(),
+          }))}
+          background="glass"
+        />
+        <button type="button" className={styles.ChooseTechnology__btn}>
+          <Link href="chooseDifficulty">Choose difficulty</Link>
+        </button>
       </div>
-      <Options
-        className={`${styles.ChooseTechnology__options}`}
-        cardStyles="withImage"
-        onChange={(e) => {
-          dispatch({ type: "SET_TECHNOLOGY", technology: e.target.value });
-        }}
-        options={technology?.map((tech) => ({
-          label: tech.name,
-          subLabel: tech.summary,
-          image: `${tech.image}.png`,
-          value: tech.name.toLowerCase(),
-        }))}
-        background="glass"
-      />
-      <button type="button" className={styles.ChooseTechnology__btn}>
-        <Link href="chooseDifficulty">Choose difficulty</Link>
-      </button>
-    </div>
+    </Layout>
   );
 }
 
