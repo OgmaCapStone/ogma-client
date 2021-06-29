13a6c673989e23583a8e8d659772d59e901ade40
Juan Daniel Martinez <mtzn_juandaniel@yahoo.com.mx>
2021-06-29 15:29:43 -0600

    ➕ ADD: alert at submit. Displaying an alert when a difficulty isn't selected. Also, changing the next/link component for a function to redirect whether a difficulty is selected.

--- pages/chooseDifficulty.js
+++ pages/chooseDifficulty.js
@@ -1,26 +1,49 @@
-import React, { useEffect, useContext } from "react";
-import Link from "next/link";
+import React, { useEffect, useContext, useState } from "react";
+import dynamic from "next/dynamic";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import Layout from "@components/Layout";
 import { store } from "@context";
 import Options from "@components/SelectOptions";
 import withAuth from "@auth";
-import { DotListIcon } from "@icons";
+import { DotListIcon, ErrorIcon } from "@icons";
 import styles from "@styles/ChooseDifficulty.module.scss";
+
+const Toast = dynamic(() => import("@components/Toast"));
 
 function chooseDifficulty() {
   const { dispatch, state } = useContext(store);
   const router = useRouter();
+  const [selectedDifficult, setSelectedDifficult] = useState("");
+  const [alert, setAlert] = useState(false);
 
   useEffect(() => {
     if (!state.technology) router.replace("/chooseTechnology");
   }, []);
 
+  function handleChange(e) {
+    const { value } = e.target;
+
+    setSelectedDifficult(value);
+    dispatch({ type: "SET_LEVEL", level: value });
+  }
+
+  function handleSubmit() {
+    if (selectedDifficult) {
+      router.replace("/questions");
+    } else {
+      setAlert(true);
+
+      setTimeout(() => {
+        setAlert(false);
+      }, 3000);
+    }
+  }
+
   return (
     <Layout>
       <Head>
-        <title>Ogma App</title>
+        <title>Choose Difficulty</title>
         <meta
           name="description"
           content="Web app to practice for yout next job interview"
@@ -33,9 +56,7 @@ function chooseDifficulty() {
         </div>
         <div className={styles.ChooseDifficulty__CardContainer}>
           <Options
-            onChange={(e) => {
-              dispatch({ type: "SET_LEVEL", level: e.target.value });
-            }}
+            onChange={handleChange}
             options={[
               {
                 label: "Junior",
@@ -61,10 +82,29 @@ function chooseDifficulty() {
             background="glass"
           />
         </div>
-        <button type="button" className={styles.ChooseDifficulty__btn}>
-          <Link href="/questions">Go to test</Link>
+        <button
+          type="button"
+          className={styles.ChooseDifficulty__btn}
+          onClick={handleSubmit}
+        >
+          Go to test
         </button>
       </div>
+      {alert && (
+        <Toast
+          toastList={[
+            {
+              backgroundColor: "#d8000c",
+              title: "Error!",
+              description: "You must choose a difficulty first!",
+              icon: <ErrorIcon />,
+            },
+          ]}
+          position="top-left"
+          autoDelete
+          autoDeleteTime={3000}
+        />
+      )}
     </Layout>
   );
 }
