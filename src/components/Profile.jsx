11a85778fa1678739a9f88503e9e50cdb2f250ee
Juan Daniel Martinez <mtzn_juandaniel@yahoo.com.mx>
2021-06-29 01:18:56 -0600

    ➕ ADD: Connecting profile to chooseTechnology. Connecting profile page to chooseTechnology page with a link button.

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,30 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import { useSession } from 'next-auth/client'
-import { getUserByEmail } from '@database/users';
-import Layout from '@components/Layout';
-import TechModal from '@components/TechModal'
-import EditButton from '@components/EditButton'
-import styles from '@styles/Profile.module.scss';
+import React, { useEffect, useState } from "react";
+import { useSession } from "next-auth/client";
+import { getUserByEmail } from "@database/users";
+import Link from "next/link";
+import Layout from "@components/Layout";
+import TechModal from "@components/TechModal";
+import EditButton from "@components/EditButton";
+import styles from "@styles/Profile.module.scss";
 
 const Profile = () => {
-  const [session, loading] = useSession()
-  const [user, setUser] = useState([])
+  const [session, loading] = useSession();
+  const [user, setUser] = useState([]);
   const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     if (!loading && session) {
-      getUserByEmail(session.user.email)
-        .then(res => setUser(res.response))
+      getUserByEmail(session.user.email).then((res) => setUser(res.response));
     }
-  }, [])
+  }, []);
 
   return (
     <Layout header footer>
       <section className={styles.profile_container}>
-        <section className={styles.profile_header}>
-          {/* <img src="/images/double-bubble.png" alt="bg-pattern" id={styles.bg_pattern}/> */}
-        </section>
-        <img src="/images/Default.jpg" id={styles.profile_pic} alt="profile-img" />
+        <section className={styles.profile_header} />
+        <img
+          src="/images/Default.jpg"
+          id={styles.profile_pic}
+          alt="profile-img"
+        />
         <section className={styles.profile_content}>
           <section className={styles.profile_edit}>
             <EditButton />
@@ -37,17 +39,21 @@ const Profile = () => {
             <span id={styles.techs}>Insert prefered technologies here!</span>
           </section>
           <section className={styles.profile_btn}>
-            <button type="button" className={styles.start_btn}>Start now</button>
+            <button type="button" className={styles.start_btn}>
+              <Link href="/chooseTechnology">Start now</Link>
+            </button>
           </section>
         </section>
       </section>
       <section className={styles.skills_container}>
         <h1>Skills</h1>
-        <button type="button" onClick={() => setShowModal(true)}>Open</button>
+        <button type="button" onClick={() => setShowModal(true)}>
+          Open
+        </button>
         {showModal && <TechModal hideModal={setShowModal} />}
       </section>
     </Layout>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
