2fef8fd25a6a411242634e9f364d43c4ccba0b2b
Juan Camilo Garcés <camo0402@gmail.com>
2021-06-29 10:41:01 -0500

    ☠️ DELETE: Toast component test at chooseDifficulty page. Nothing functional, just a componetnt's visual test.

--- pages/chooseDifficulty.js
+++ pages/chooseDifficulty.js
@@ -2,38 +2,11 @@ import React, { useState, useContext } from "react";
 import Link from "next/link";
 import { store } from "@context";
 import Options from "@components/SelectOptions";
-import Toast from "@components/Toast";
 import withAuth from "@auth";
 import styles from "@styles/ChooseDifficulty.module.scss";
 
 function chooseDifficulty() {
   const { dispatch } = useContext(store);
-  const testList = [
-    {
-      id: 1,
-      title: "Success",
-      description: "This is a success toast component",
-      backgroundColor: "#5cb85c",
-    },
-    {
-      id: 2,
-      title: "Danger",
-      description: "This is an error toast component",
-      backgroundColor: "#d9534f",
-    },
-    {
-      id: 3,
-      title: "Info",
-      description: "This is an info toast component",
-      backgroundColor: "#5bc0de",
-    },
-    {
-      id: 4,
-      title: "Warning",
-      description: "This is a warning toast component",
-      backgroundColor: "#f0ad4e",
-    },
-  ];
 
   return (
     <div className={styles.ChooseDifficulty}>
@@ -70,7 +43,6 @@ function chooseDifficulty() {
       <button type="button" className={styles.ChooseDifficulty__btn}>
         <Link href="/questions">Go to test</Link>
       </button>
-      <Toast toastList={testList} position="bottom-right" />
     </div>
   );
 }
